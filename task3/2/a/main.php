03d39e68b0bde25f97852182e8955d8b70d34702
my_name <coolthingstv1337@gmail.com>
2022-08-21 17:21:15 +0300

    indents

--- task3/2/a/main.php
+++ task3/2/a/main.php
@@ -1,11 +1,11 @@
 <?php
 
-  if (!isset($_POST["textBody"])) {
-    http_response_code(400);
-    return;
-  }
+if (!isset($_POST["textBody"])) {
+  http_response_code(400);
+  return;
+}
 
-  $textBody = $_POST["textBody"];
-  echo "Words: " . str_word_count($textBody) . "<br>Chars: " . strlen($textBody);
+$textBody = $_POST["textBody"];
+echo "Words: " . str_word_count($textBody) . "<br>Chars: " . strlen($textBody);
 
 ?>
--- task3/2/b/display_data.php
+++ task3/2/b/display_data.php
@@ -1,8 +1,8 @@
 <?php
 
-  $data_set = isset($_SESSION["name"]) && isset($_SESSION["surname"]) && isset($_SESSION["age"]);
-  if ($data_set) {
-    echo "{$_SESSION['name']} {$_SESSION['surname']} {$_SESSION['age']}";
-  }
+$data_set = isset($_SESSION["name"]) && isset($_SESSION["surname"]) && isset($_SESSION["age"]);
+if ($data_set) {
+  echo "{$_SESSION['name']} {$_SESSION['surname']} {$_SESSION['age']}";
+}
 
 ?>
